1c7de5aa869ff3d425fb147df752ebc749b4153b
Ayush Kumar <wemayush@gmail.com>
2025-03-26 23:51:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,29 +24,29 @@ const Footer = () => {
           <div className="social-links">
             <a href="https://fb.openinapp.co/slva6" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FontAwesomeIcon icon={faFacebookF} />
-            </a>
+            </a>              
             <a href="https://insta.openinapp.co/x2h0y" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FontAwesomeIcon icon={faInstagram} />
-            </a>
+            </a>                  
             <a href="https://yt.openinapp.co/einv7" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FontAwesomeIcon icon={faYoutube} />
-            </a>
+            </a>                  
             <a href="https://wa.me/9472548097" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FontAwesomeIcon icon={faWhatsapp} />
-            </a>
-          </div>
-        </div>
+            </a>    
+          </div>    
+        </div>    
 
         <div className="footer-section">
           <h4>Quick Links</h4>
-          <ul>
+          <ul>    
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/classes">Classes</Link></li>
             <li><Link to="/schedule">Schedule</Link></li>
             <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </div>
+          </ul>                                                          
+        </div>                                                          
 
         <div className="footer-section">
           <h4>Contact Info</h4>
@@ -55,8 +55,8 @@ const Footer = () => {
             <li>Gopalganj</li>
             <li>Phone: +91 9472548097</li>
             <li>Email: info@pulsegym.com</li>
-          </ul>
-        </div>
+          </ul>                                                                      
+        </div>                                                                      
 
         <div className="footer-section">
           <h4>Opening Hours</h4>
@@ -64,15 +64,26 @@ const Footer = () => {
             <li>Monday - Friday: 6:00 AM - 10:00 PM</li>
             <li>Saturday: 7:00 AM - 8:00 PM</li>
             <li>Sunday: 8:00 AM - 6:00 PM</li>
-          </ul>
-        </div>
-      </div>
+          </ul>                                                                  
+        </div>                                                                  
+      </div>                                                                  
 
       <div className="footer-bottom">
         <p>&copy; {new Date().getFullYear()} Pulse Gym. All rights reserved.</p>
-      </div>
+       <p>                                                                                                          
+              <a                                                                                                    
+                href="https://www.instagram.com/ayush_ix_xi/?hl=en"
+                target="_blank"                                                                                                    
+                rel="noopener noreferrer"                                                                                    
+                className="footer-link"
+              >
+                dev.ayush
+              </a>                                                                                    
+            </p>                                                                                    
+             
+      </div>                                                              
     </footer>
-  );
-};
+  );                                                      
+};                                                      
 
 export default Footer; 
